4238ef75e2d9bd6401796c642044c9bb1af458be
Pascal Süß <pascal.suess@googlemail.com>
2023-05-15 19:39:27 +0000

    moved bg images to correct folder and made outline for name visibility

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,15 +20,17 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+import image from "../images/japanese-garden-chimes.jpg"; //alt: hishi-odoshi.jpg or japanese-garden-stones.jpg
+
+const imageAltText = "Japanese garden with stones stacked in a pile";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", 
+      color:"white", textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"}}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
